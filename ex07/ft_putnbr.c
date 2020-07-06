791a21be90ee132cc370347ee27334560d1071ae
hemin <hemin@student.42seoul.kr>
2020-07-06 16:56:08 +0900

    third commit

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemin <hemin@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/06 14:55:30 by hemin             #+#    #+#             */
+/*   Updated: 2020/07/06 16:54:12 by hemin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int 	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * (-1));
+	}
+	else
+	{
+		if (nb >= 10)
+			{
+				ft_putnbr(nb / 10);
+			}
+		ft_putchar((nb % 10) + '0');
+
+	}
+}
