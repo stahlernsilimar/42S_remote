245285dc01b0fd11e355bbbf8b295b3ca8e068bf
hemin <hemin@student.42seoul.kr>
2020-07-06 12:58:32 +0900

    modified

--- ex05/ft_print_comb.c
+++ ex05/ft_print_comb.c
@@ -6,25 +6,24 @@
 /*   By: hemin <hemin@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/05 17:12:05 by hemin             #+#    #+#             */
-/*   Updated: 2020/07/06 12:46:34 by hemin            ###   ########.fr       */
+/*   Updated: 2020/07/06 12:57:59 by hemin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int 				ft_putchar(char c)
+int					ft_putchar(char c)
 {
 					write(1, &c, 1);
 	return (0);
 }
 
-void 				ft_print_comb(void)
+void				ft_print_comb(void)
 {
 	int a;
 	int b;
 	int c;
 	a = 0 + '0';
-
 	while (a++ < '7')
 	{
 		b = a;
@@ -33,9 +32,9 @@ void 				ft_print_comb(void)
 			c = b;
 			while (c++ < '9')
 			{
-					ft_putchar(a);
-					ft_putchar(b);
-					ft_putchar(c);
+				ft_putchar(a);
+				ft_putchar(b);
+				ft_putchar(c);
 				if (a != '7')
 				{
 					ft_putchar(',');
@@ -44,9 +43,4 @@ void 				ft_print_comb(void)
 			}
 		}
 	}
-}
-
-int main()
-{
-	ft_print_comb();
 }
