9f0c07c013fdffa901af7e75eb0d48bc4e2e388a
hemin <hemin@student.42seoul.kr>
2020-07-16 02:28:31 +0900

    ex11 - modified

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -15,30 +15,26 @@
 void	ft_putstr_non_printable(char *str)
 {
 	int	pos_str;
-	char digit[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-	char c;
+	char digit[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 	int c1;
 	int c2;
+	char c;
 
-	pos_str = 0;
 	c = '\\';
+	pos_str = 0;
 	while(str[pos_str] != '\0')
 	{
 		if ((str[pos_str] >= ' ') && (str[pos_str] <= '~'))
-		{
 			write(1, &str[pos_str], 1);
-			pos_str++;
-		}
-		else
+				else
 		{
-			c1 = str[pos_str] / 16;
-			c2 = str[pos_str] % 16;
-
+			c1 = (unsigned)str[pos_str] / 16;
+			c2 = (unsigned)str[pos_str] % 16;
 			write(1, &c, 1);
 			write(1, &digit[c1] , 1);
 			write(1, &digit[c2] , 1);
-			pos_str++;
 		}
+		pos_str++;
 	}
 }
 
