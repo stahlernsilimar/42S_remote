f0aefe4cfa99dbeab6234f292370f757a9c77668
hemin <hemin@student.42seoul.kr>
2020-07-21 03:39:32 +0900

    co4

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -11,61 +11,64 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void	ft_putnbr(int nb, char *base)
+void	ft_putnbr(int nb, char *base, int digit)
 {
-	int ft_left = 0;
+	int ft_left;
+
+	ft_left = 0;
 
 	if (nb == -2147483648)
 	{
-		write(1, "-2", 2);
-		ft_putnbr(147483648, base);
+		ft_left = (nb % digit) * (-1);
+		ft_putnbr(nb / digit, base, digit);
+		write(1, &base[ft_left], 1);
 	}
 	else if (nb < 0)
 	{
 		write(1, "-", 1);
-		ft_putnbr(nb * (-1), base);
+		ft_putnbr(nb * (-1), base, digit);
 	}
-	else if (nb >= 10)
+	else if ((nb / digit) != 0)
 	{
-		ft_left = nb % 10;
-		ft_putnbr(nb / 10, base);
-		ft_putchar(base[ft_left]);
-		printf("%d", ft_left);
+		ft_left = nb % digit;
+		ft_putnbr(nb / digit, base, digit);
+		write(1, &base[ft_left], 1);
 	}
 	else
-		ft_putchar(base[ft_left]);
+	{
+		ft_left = nb % digit;
+		write(1, &base[ft_left], 1);
+	}
 }
 
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int base_size;
-	int base_size_test;
-	int neg;
-	int count;
+	int i;
+	int j;
 
-	base_size = 0;
-	neg = 0;
-	count = 0;
-	while (base[base_size] != '\0')
+	i = 0;
+	while (base[i] != '\0')
 	{
-		if ((base[base_size] == '+') || (base[base_size] == '-') ||
-		((base[base_size] > 8) && (base[base_size] < 14)) || (base[base_size] == ' '))
+		if ((base[i] == '+') || (base[i] == '-') ||
+		((base[i] > 8) && (base[i] < 14)) || (base[i] == ' '))
 			return ;
-		base_size++;
-		base_size_test = base_size;
-			while (base[base_size_test] != '\0')
-			{
-				if(base[base_size] == base[base_size_test])
-					return ;
-			}
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if(base[i] == base[j])
+				return ;
+			j++;
+		}
+		i++;
 	}
-	if (base_size < 2)
+	if (i < 2)
 		return ;
-	ft_putnbr(nbr, base);
+	ft_putnbr(nbr, base, i);
 }
---
+++ c04/ex05/ft_atoi_base.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemin <hemin@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/21 02:30:45 by hemin             #+#    #+#             */
+/*   Updated: 2020/07/21 02:30:45 by hemin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_putnbr(int nb, char *base, int digit, int total)
+{
+	int ft_left;
+
+	ft_left = 0;
+	if (nb == -2147483648)
+	{
+		ft_left = (nb % digit) * (-1);
+		total = ft_putnbr(nb / digit, base, digit, total);
+		total = total * 10 + (base[ft_left] - '0');
+	}
+	else if (nb < 0)
+	{
+		total = ft_putnbr(nb * (-1), base, digit, total);
+	}
+	else if ((nb / digit) != 0)
+	{
+		ft_left = nb % digit;
+		total = ft_putnbr(nb / digit, base, digit, total);
+		total = total * 10 + (base[ft_left] - '0');
+	}
+	else
+	{
+		ft_left = nb % digit;
+		total = total * 10 + (base[ft_left] - '0');
+	}
+	return (total);
+}
+
+int	ft_atoi(char *str)
+{
+	int count;
+	int i;
+	int ret;
+
+	count = 0;
+	i = 0;
+	ret = 0;
+	while (str[i] != '\0' && (((str[i] > 8) && (str[i] < 14)) || str[i] == ' '))
+	{
+		i++;
+	}
+	while (str[i] != '\0' && (str[i] == '+' || str[i] =='-'))
+	{
+		if (str[i] == '-')
+			count++;
+		i++;
+	}
+	while(str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		if ((str[i] >= '0') && (str[i] <= '9'))
+			ret = (ret * 10) + (str[i] - '0');
+		i++;
+	}
+	if (count % 2 == 1)
+		ret = ret * (-1);
+	return (ret);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int i;
+	int j;
+	int nbr;
+	int total;
+	int neg;
+
+	if (str[0] == '\0')
+		return (0);
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if ((base[i] == '+') || (base[i] == '-') ||
+		((base[i] > 8) && (base[i] < 14)) || (base[i] == ' '))
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if(base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	if (i < 2)
+		return (0) ;
+	total = 0;
+	neg = 0;
+	nbr = ft_atoi(str);
+	if (nbr < 0)
+		neg = 1;
+	nbr = ft_putnbr(nbr, base, i, total);
+	if (neg == 1)
+		return (nbr * (-1));
+	else
+		return (nbr);
+}
+
+int main()
+{
+	printf("%d ", ft_atoi_base("42", "0123456789"));
+	printf("%d ", ft_atoi_base("   42abc", "0123456789"));
+	printf("%d ", ft_atoi_base("  +--++-rcrdmddd+-abc", "mrdoc"));
+	printf("%d ", ft_atoi_base("-10000000000000000000000000000000", "01"));
+	printf("%d ", ft_atoi_base("8888888888888888888888888888888", "a8"));
+	printf("%d ", ft_atoi_base("one", "e"));
+	printf("%d ", ft_atoi_base("blank", ""));
+	printf("%d ", ft_atoi_base("", "za"));
+	printf("%d ", ft_atoi_base("ca", "zaca"));
+	printf("%d ", ft_atoi_base("ca", "z\v\f\nac"));
+	printf("\n");
+}
